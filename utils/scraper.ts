b64a4b42976f50ad7f224e7895ba7ada9d664c7f
idoMadarr <idox2x@gmail.com>
2025-04-01 12:37:16 +0300

    try-catch

--- utils/scraper.ts
+++ utils/scraper.ts
@@ -1,8 +1,8 @@
-import { ElementHandle, Page } from 'puppeteer';
-import node from 'node:timers/promises';
-import selectors from './selectors';
-import { ItemType } from '../types';
-import BotAutomation from '../models/BotAutomation';
+import { ElementHandle, Page } from "puppeteer";
+import node from "node:timers/promises";
+import selectors from "./selectors";
+import { ItemType } from "../types";
+import BotAutomation from "../models/BotAutomation";
 
 export const onScraper = async (page: Page, bot: BotAutomation) => {
   const items = await page.$$(selectors.general.items);
@@ -11,22 +11,22 @@ export const onScraper = async (page: Page, bot: BotAutomation) => {
   for (const item of items) {
     const title = await item.$eval(
       selectors.general.title,
-      el => el.textContent?.trim() || null
+      (el) => el.textContent?.trim() || null
     );
     const price = await item.$eval(
       selectors.general.price,
-      el => el.textContent || null
+      (el) => el.textContent || null
     );
     const image = await item.$eval(
       selectors.general.image,
-      el => el.getAttribute('src') || null
+      (el) => el.getAttribute("src") || null
     );
     const href = await item.$eval(
       selectors.general.href,
-      el => el.getAttribute('href') || null
+      (el) => el.getAttribute("href") || null
     );
     const navigateLink = `https://www.clubhub.co.il${href}`;
-    let purchasableLink = '';
+    let purchasableLink = "N/a";
 
     if (href) purchasableLink = await fetchNestedHref(page, navigateLink);
     const product = { title, price, image, href: purchasableLink };
@@ -39,24 +39,37 @@ export const onScraper = async (page: Page, bot: BotAutomation) => {
 };
 
 const fetchNestedHref = async (page: Page, navigateLink: string) => {
-  let purchasableLink = '';
-  await page.goto(navigateLink);
-  await node.setTimeout(1000);
-  await page.waitForSelector(selectors.general.urls);
-  const linkList = (await page.$$(
-    selectors.general.urls
-  )) as ElementHandle<HTMLAnchorElement>[];
+  let purchasableLink = "";
 
-  for (const el of linkList) {
-    const href = await el.evaluate(el => el.href || '');
-    if (href?.includes('paisplus')) {
-      purchasableLink = href;
+  try {
+    await page.goto(navigateLink);
+    await node.setTimeout(1000);
+    await page.waitForSelector(selectors.general.urls, { timeout: 3000 });
+    const linkList = (await page.$$(
+      selectors.general.urls
+    )) as ElementHandle<HTMLAnchorElement>[];
+
+    for (const el of linkList) {
+      const href = await el.evaluate((el) => el.href || "");
+      if (href?.includes("paisplus")) {
+        purchasableLink = href;
+      }
     }
+  } catch (error) {
+    console.error(
+      `Skipping broken link: ${navigateLink}, error: ${JSON.stringify(error)}`
+    );
   }
 
-  await page.goBack();
-  await page.waitForSelector(selectors.general.items);
-  await page.$$(selectors.general.items);
+  try {
+    await page.goBack();
+    await page.waitForSelector(selectors.general.items);
+    await page.$$(selectors.general.items);
+  } catch (error) {
+    console.warn(
+      `Could not go back to main page after broken link: ${navigateLink}`
+    );
+  }
 
   return purchasableLink;
 };
